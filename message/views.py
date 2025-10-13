ed2a76ecc93c7e97e103edcaf846d6bc845bde67
neetharmaliackal <neethamaliackal@gmail.com>
2025-10-13 14:40:01 +1300

    created send message.

--- message/views.py
+++ message/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+
+from rest_framework import status
+from rest_framework.response import Response
+
+from message.models import Message
+from rest_framework.decorators import api_view
+
+from message.serializers import MessageSerializer
+
+
+@api_view(['POST'])
+def send_message(request):
+    message_serializer = MessageSerializer(data=request.data)
+    if message_serializer.is_valid():
+        message_serializer.save()
+        return Response({'message': 'Message sent successfully'},
+                        status=status.HTTP_201_CREATED)
+    return None
