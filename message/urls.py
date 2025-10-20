13e29172b003c23bcb58c9cd5de6cc06664feee1
neetharmaliackal <neethamaliackal@gmail.com>
2025-10-20 14:47:06 +1300

    logout views created test passed

--- message/urls.py
+++ message/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from message.views import send_message, LoginView
+from message.views import send_message, LoginView, LogoutView
 from message.viewsets import Chat_roomViewSet, MessageViewSet
 
 router=DefaultRouter()
@@ -29,5 +30,6 @@ router.register(r'messages', MessageViewSet,basename='message')
 
 urlpatterns =[path('',include(router.urls)),
               path('send_message/',send_message,name='send_message'),
-              path('login/',LoginView.as_view(),name='login')]
+              path('login/',LoginView.as_view(),name='login'),
+              path('logout/',LogoutView.as_view(),name='logout')]
 
