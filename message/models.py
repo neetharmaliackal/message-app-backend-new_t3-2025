d5335d895f2be01a61d172e04b4a0be52697ebec
neetharmaliackal <neethamaliackal@gmail.com>
2025-10-20 15:42:53 +1300

    chat_rooms created , testing passed

--- message/models.py
+++ message/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 class Chat_room(models.Model):
     name = models.CharField(max_length=100)
+    users=models.ManyToManyField(User,related_name="chat_rooms")
+
 # Create your models here.
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
--- message/tests.py
+++ message/tests.py
@@ -4,7 +4,6 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Message, Chat_room
 
-
 class SendMessageAPITest(APITestCase):
     def setUp(self):
         # Create test users
@@ -56,10 +55,11 @@ class SendMessageAPITest(APITestCase):
         self.assertIn('content', response.data)
 
 
-class LoginApITest(APITestCase):
+class LoginAPITest(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpass')
         self.url = reverse('login')
+
     def test_login_success(self):
         response = self.client.post(self.url, {'username': 'testuser', 'password': 'testpass'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -68,6 +68,7 @@ class LoginApITest(APITestCase):
     def test_login_invalid(self):
         response = self.client.post(self.url, {'username': 'testuser', 'password': 'wrongpass'})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('non_field_errors', response.data)
 
 
 class LogoutAPITest(APITestCase):
@@ -81,6 +82,7 @@ class LogoutAPITest(APITestCase):
         login_response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpass'})
         token = login_response.data.get('token')
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+
         # Logout with token
         response = self.client.post(self.logout_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -90,3 +92,39 @@ class LogoutAPITest(APITestCase):
         response = self.client.post(self.logout_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
+class ChatRoomViewSetTest(APITestCase):
+    def setUp(self):
+        # Create users
+        self.user1 = User.objects.create_user(username='user1', password='pass123')
+        self.user2 = User.objects.create_user(username='user2', password='pass123')
+
+        # Create chat rooms
+        self.chatroom1 = Chat_room.objects.create(name='Room A')
+        self.chatroom1.users.add(self.user1)
+
+        self.chatroom2 = Chat_room.objects.create(name='Room B')
+        self.chatroom2.users.add(self.user2)
+
+        self.chatroom3 = Chat_room.objects.create(name='Room C')
+        self.chatroom3.users.add(self.user1, self.user2)
+
+        self.login_url = reverse('login')
+        self.url = reverse('chat_rooms-list')  # standard DRF naming for ViewSets
+
+    def test_authenticated_user_sees_only_their_rooms(self):
+        """User should only see chat rooms they belong to."""
+        login_response = self.client.post(self.login_url, {'username': 'user1', 'password': 'pass123'})
+        token = login_response.data.get('token')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+        response = self.client.get(self.url)
+
+        room_names = [room['name'] for room in response.data]
+        self.assertIn('Room A', room_names)
+        self.assertIn('Room C', room_names)
+        self.assertNotIn('Room B', room_names)
+
+    def test_unauthenticated_user_cannot_access(self):
+        """Unauthenticated users should get 401."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
